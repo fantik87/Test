e54fa21db8c66d82e55561bdd764bda56b8f09c9
fantik87 <94090333+fantik87@users.noreply.github.com>
2023-05-19 06:48:26 +0500

    Second Commit

---
+++ TestGame/Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] public float bulletLifetime = 3f;
+    void Start()
+    {
+
+    }
+
+    public void SpawnBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = spawnPoint.position * bulletSpeed;
+        Debug.Log("hui");
+
+        Destroy(bullet, bulletLifetime);
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
